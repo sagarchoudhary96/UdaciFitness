504aa5419a2cff3f1de601140d269273e36ef161
Sagar Choudhary <sagar.choudhary96@gmail.com>
2017-12-03 20:42:24 +0530

    update: components

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
-import { getMetricMetaInfo } from '../utils/helper'
+import { View, TouchableOpacity, Text } from 'react-native'
+import { getMetricMetaInfo, timeToString } from '../utils/helper'
 import Uslider from './Uslider'
 import Usteppers from './Usteppers'
 import DateHeader from './DateHeader'
+import {Ionicons} from '@expo/vector-icons'
+import TextButton from './TextButton'
+
+
+function SubmitBtn ({onPress}) {
+  return (
+    <TouchableOpacity
+      onPress={onPress}>
+      <Text>SUBMIT</Text>
+    </TouchableOpacity>
+  )
+}
 
 export default class AddEntry extends Component {
 
@@ -40,11 +52,62 @@ export default class AddEntry extends Component {
 
   slide = (metric, val) => {
     this.setState({
-      [metric]: value
+      [metric]: val
     })
   }
+
+  submit = () => {
+    const key = timeToString()
+    const entry = this.state
+
+    //clear the state
+    this.setState({
+      run: 0,
+      bike: 0,
+      swim: 0,
+      sleep: 0,
+      eat: 0,
+    })
+    //update redux
+    //navigate to home
+    //save to database
+
+    //clear notification
+  }
+
+  reset = () => {
+    const key = timeToString()
+
+    //clear the state
+    this.setState({
+      run: 0,
+      bike: 0,
+      swim: 0,
+      sleep: 0,
+      eat: 0,
+    })
+    //update redux
+    //navigate to home
+    //save to database
+
+    //clear notification
+
+  }
+
   render () {
     const metaInfo = getMetricMetaInfo()
+
+    if (this.props.alreadyLogged) {
+      return (
+        <View>
+          <Ionicons
+            name='ios-happy-outline'
+            size={100}/>
+          <Text>You already logged your information for today.</Text>
+          <TextButton onPress={this.reset}>RESET</TextButton>
+        </View>
+      )
+    }
     return (
       <View>
         <DateHeader date={(new Date()).toLocaleDateString()} />
@@ -71,6 +134,7 @@ export default class AddEntry extends Component {
             </View>
           )
         })}
+        <SubmitBtn onPress={this.submit}/>
       </View>
     )
   }
